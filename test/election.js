43455a09c57017a455a799bab94eb4a41b66fd3b
rajkumar patoliya <patoliyaraj@gmail.com>
2019-01-01 16:14:48 +0530

    added first test initializes twocandidates

--- test/election.js
+++ test/election.js
@@ -1,2 +1,12 @@
 /* jshint esversion: 6 */
 let Election = artifacts.require("./Election.sol");
+
+contract("Election", (accounts) => {
+    it("Initializes with two candidates", () => {
+        return Election.deployed().then((instance) => {
+            return instance.candidatesCount();
+        }).then((count) => {
+            assert.equal(count, 2);
+        });
+    });
+});
