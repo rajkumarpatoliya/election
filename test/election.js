7a4c36bfa0a7243c422abff98d3251811200363c
rajkumar patoliya <patoliyaraj@gmail.com>
2019-01-02 15:13:34 +0530

    under development added test cases

--- test/election.js
+++ test/election.js
@@ -29,7 +29,7 @@ contract("Election", (accounts) => {
         });
     });
 
-    it("Allows a voter to cast a vote", ()=> {
+    it("Allows a voter to cast a vote",() => {
         return Election.deployed().then((instance) => {
             electionInstance = instance;
             candidateId=1;
@@ -44,4 +44,46 @@ contract("Election", (accounts) => {
             assert.equal(voteCount, 1, "Increments the candidate's vote count");
         });
     });
+
+    it("throws an exception for invalid candiates",() => {
+        return Election.deployed().then((instance) => {
+          electionInstance = instance;
+          return electionInstance.vote(99, { from: accounts[1] })
+        }).then(assert.fail).catch((error) => {
+          assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
+          return electionInstance.candidates(1);
+        }).then((candidate1) => {
+          var voteCount = candidate1[2];
+          assert.equal(voteCount, 1, "Shri Narendra Modiji did not receive any votes");
+          return electionInstance.candidates(2);
+        }).then((candidate2) => {
+          var voteCount = candidate2[2];
+          assert.equal(voteCount, 0, "Shri Priyanka Gandhi did not receive any votes");
+        });
+      });
+    
+      it("throws an exception for double voting", () => {
+        return Election.deployed().then((instance) => {
+          electionInstance = instance;
+          candidateId = 2;
+          electionInstance.vote(candidateId, { from: accounts[1] });
+          return electionInstance.candidates(candidateId);
+        }).then((candidate) => {
+          var voteCount = candidate[2];
+          assert.equal(voteCount, 1, "accepts first vote");
+          // Try to vote again
+          return electionInstance.vote(candidateId, { from: accounts[1] });
+        }).then(assert.fail).catch((error) => {
+          assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
+          return electionInstance.candidates(1);
+        }).then((candidate1) => {
+          var voteCount = candidate1[2];
+          assert.equal(voteCount, 1, "Shri Narandra Modiji did not receive any votes");
+          return electionInstance.candidates(2);
+        }).then((candidate2) => {
+          var voteCount = candidate2[2];
+          assert.equal(voteCount, 1, "Shri Priyanka Gandhi did not receive any votes");
+        });
+      });
+   
 });
