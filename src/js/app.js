479e8b5e61d2ad0e33658648a0e3aec1032fe7bb
rajkumar patoliya <patoliyaraj@gmail.com>
2019-01-02 12:33:13 +0530

    under development minor changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,20 @@
+/* jshint esversion: 6 */
 App = {
   web3Provider: null,
   contracts: {},
   account: '0x0',
 
-  init: function() {
+  init: () => {
     return App.initWeb3();
   },
 
-  initWeb3: function() {
-    // TODO: refactor conditional
+  initWeb3: () => {
     if (typeof web3 !== 'undefined') {
-      // If a web3 instance is already provided by Meta Mask.
+  
       App.web3Provider = web3.currentProvider;
       web3 = new Web3(web3.currentProvider);
     } else {
-      // Specify default instance if no web3 instance provided
+
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
       web3 = new Web3(App.web3Provider);
     }
@@ -22,7 +22,7 @@ App = {
   },
 
   initContract: function() {
-    $.getJSON("Election.json", function(election) {
+    $.getJSON("Election.json",(election) => {
       // Instantiate a new truffle contract from the artifact
       App.contracts.Election = TruffleContract(election);
       // Connect provider to interact with contract
@@ -32,7 +32,7 @@ App = {
     });
   },
 
-  render: function() {
+  render:() => {
     var electionInstance;
     var loader = $("#loader");
     var content = $("#content");
@@ -41,7 +41,7 @@ App = {
     content.hide();
 
     // Load account data
-    web3.eth.getCoinbase(function(err, account) {
+    web3.eth.getCoinbase((err, account) => {
       if (err === null) {
         App.account = account;
         $("#accountAddress").html("Your Account: " + account);
@@ -49,15 +49,15 @@ App = {
     });
 
     // Load contract data
-    App.contracts.Election.deployed().then(function(instance) {
+    App.contracts.Election.deployed().then((instance) => {
       electionInstance = instance;
       return electionInstance.candidatesCount();
-    }).then(function(candidatesCount) {
+    }).then((candidatesCount) => {
       var candidatesResults = $("#candidatesResults");
       candidatesResults.empty();
 
       for (var i = 1; i <= candidatesCount; i++) {
-        electionInstance.candidates(i).then(function(candidate) {
+        electionInstance.candidates(i).then((candidate) => {
           var id = candidate[0];
           var name = candidate[1];
           var voteCount = candidate[2];
@@ -70,14 +70,14 @@ App = {
 
       loader.hide();
       content.show();
-    }).catch(function(error) {
+    }).catch((error) => {
       console.warn(error);
     });
   }
 };
 
-$(function() {
-  $(window).load(function() {
+$(() => {
+  $(window).load(() => {
     App.init();
   });
 });
