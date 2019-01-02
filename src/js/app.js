bfa223ed85b3279b4a9e2a33cbb14f5a11da04af
rajkumar patoliya <patoliyaraj@gmail.com>
2019-01-02 11:04:35 +0530

    added app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,68 +1,35 @@
 App = {
   web3Provider: null,
   contracts: {},
+  account:'0x0',
 
-  init: async function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
-
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-
-        petsRow.append(petTemplate.html());
-      }
-    });
-
-    return await App.initWeb3();
+  init: async () => {
+    return App.initWeb3();
   },
+  initWeb3: () => {
+    if(typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(App.web3Provider);
 
-  initWeb3: async function() {
-    /*
-     * Replace me...
-     */
-
+    } else {
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      web3 = new Web3(App.web3Provider);
+    }
     return App.initContract();
   },
 
-  initContract: function() {
-    /*
-     * Replace me...
-     */
+  initContract: () => {
+    $.getJSON('Election.json', (election) => {
+      App.contracts.Election = TruffleContract(election);
+      App.contracts.Election.setProvider(App.web3Provider);
 
-    return App.bindEvents();
+      return App.render();
+    });
   },
 
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
+  render: () => {
+    let electionInstance;
+    let loader = $("#loader");
+    let content = $("#content");
   },
-
-  markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
-  },
-
-  handleAdopt: function(event) {
-    event.preventDefault();
-
-    var petId = parseInt($(event.target).data('id'));
-
-    /*
-     * Replace me...
-     */
-  }
-
 };
-
-$(function() {
-  $(window).load(function() {
-    App.init();
-  });
-});
